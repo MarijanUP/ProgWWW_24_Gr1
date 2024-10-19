ce40dba81b7c235a63e90123e7c05d47269e47c4
Marijanito <marijan.kolaj@student.uni-pr.edu>
2024-10-19 13:51:30 +0200

    Disa ndrrime te vogla
    I kam shly do rreshta per shkak te privatsise se databazes

--- login.js
+++ login.js
@@ -1,23 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
 import { getDatabase, ref,get, child} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-database.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCz21d0Tzykamo2rcHJ0-l-qRYSJZ429PM",
-  authDomain: "seks-f1000.firebaseapp.com",
-  databaseURL: "https://seks-f1000-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "seks-f1000",
-  storageBucket: "seks-f1000.appspot.com",
-  messagingSenderId: "1084488790517",
-  appId: "1:1084488790517:web:5e885dfa5e2d3839a3f0bc",
-  measurementId: "G-KT8PQT8XMT"
-};
-
-// Initialize Firebase
+// qtu shkon firebase stuff, nuk i kam qit per shkak te privatsise
 
 const app = initializeApp(firebaseConfig);
 
@@ -38,7 +22,7 @@ loginButton.addEventListener('click', function(){
         get(child(db, `USERS/${SID}`)).then((snapshot) => {
             if (!snapshot.exists()) {
                 alert("User does not exist!");
-            } else if (snapshot.exists() && !(snapshot.child("PASS").val()===PASS)){
+            } else if (snapshot.exists() && !snapshot.child("PASS").val()===PASS){
                 alert("Incorrect password!");
             } else if (snapshot.exists() && snapshot.child("PASS").val()===PASS){
                 window.location.href="home.html"
