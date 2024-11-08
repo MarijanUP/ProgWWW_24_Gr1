fe281002e37e5958bd966c7c1d2701c08f602982
jasinua1 <jasin.avdiu@student.uni-pr.edu>
2024-11-08 13:43:48 +0100

    Changes in header, and posts

--- javascript/fhloader.js
+++ javascript/fhloader.js
@@ -14,9 +14,14 @@ function loadLHeader(){
 }
 
 function loadHeader() {
+
+    
     fetch('header.html')
         .then(response => response.text()).then(data => {
             document.getElementById('header').innerHTML = data;
         })
     .catch(error => console.error('Error loading header:', error));
+
+
+    
 }
