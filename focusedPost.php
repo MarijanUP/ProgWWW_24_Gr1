4df3e5c466dfc8c0cfb9b598db17297cf4d05d31
Marijanoo <marijan.kolaj@student.uni-pr.edu>
2024-12-02 16:26:50 +0100

    commentSection fixed
    komentet bahen load kur qelet postimi

--- focusedPost.php
+++ focusedPost.php
@@ -23,13 +23,6 @@ if(isset($_SESSION['logged'])){
         localStorage.setItem( "bachelor" , '<?php echo $_SESSION["bachelor"]; ?>' );
         localStorage.setItem( "email" , '<?php echo $_SESSION["email"]; ?>' );
         localStorage.setItem( "profile" , '<?php echo $_SESSION["pic"]; ?>' );
-
-
-
-
-
-        
-
     </script>
 
     <script src="javascript/focusedPost.js" type="module"></script>
@@ -42,69 +35,52 @@ if(isset($_SESSION['logged'])){
 
         <div class="background">
            
-        <div id="container" style ="margin-top:50px;">
-        <div id="top">
-            <div class="cropper">
-                <img class="pfp" id="poster" src="" alt="">
-            </div>
-            <div class="name-and-time">
-                <div class="name" id="name">John Doe</div>
-                <div id="time" class="time">00/00 00:00:00</div>
-            </div>
-        </div>
+        <div id="container">
 
-        <hr>
-
-        <div id="mid">
-            <div class="title" id="title">Title</div>
-            <div class="desc" id="desc">Description</div>
-        </div>
-
-        <hr> 
-
-        <div id="uniAndTime">
-            <div id="uni">Bachelor</div>
-        </div>
-        
-        <div id="interactions">
-            <div class="count" id="likes">0</div>
-            <i id="likeButton" class="fa fa-thumbs-up" style="color:grey;"></i>
-            <div class="count" id="comments">0</div>
-            <i id="social" class="fa fa-comment"></i>
-        </div>
-
-        <div id="bottom">
-            <div class="cropper">
-                <img class="pfp" id="user" src="<?php echo $_SESSION['pic'];?>" alt="">
-            </div>
-            <input id="commentText" type="text" placeholder="Leave a comment...">
-            <input id="commentButton" type="submit" value="Comment">
-        </div>
-
-
-
-
-        </div>
-
-        <div class = utility>
-             
-              <div class="cropper">
-            <img class="pic" id='pic' src="images/jasini.jpg" alt="profilePic">
-        </div>
-        <div class="info">
-
-            <div class="nameTime">
-                <p id="name" class="name">John Doe</p>
-                <p id="time" class="time">00/00 00:00:00</p>
+            <div id="top">
+                <div class="cropper">
+                    <img class="pfp" id="poster" src="" alt="">
+                </div>
+                <div class="name-and-time">
+                    <div class="name" id="name">John Doe</div>
+                    <div id="time" class="time">00/00 00:00:00</div>
+                </div>
             </div>
 
             <hr>
 
-            <div class="notifContent" >
-                <p id='content' class="content"></p>
+            <div id="mid">
+                <div class="title" id="title">Title</div>
+                <div class="desc" id="desc">Description</div>
+            </div>
+
+            <hr> 
+
+            <div id="uniAndTime">
+                <div id="uni">Bachelor</div>
             </div>
             
+            <div id="interactions">
+                <div class="count" id="likes">0</div>
+                <i id="likeButton" class="fa fa-thumbs-up" style="color:grey;"></i>
+                <div class="count" id="comments">0</div>
+                <i id="social" class="fa fa-comment"></i>
+            </div>
+
+            <div id="bottom">
+                <div class="cropper">
+                    <img class="pfp" id="user" src="<?php echo $_SESSION['pic'];?>" alt="">
+                </div>
+                <input id="commentText" type="text" placeholder="Leave a comment...">
+                <input id="commentButton" type="submit" value="Comment">
+            </div>
+
         </div>
+
+        <div id='commentSection'>  
+
+        </div>
+        
     </body>
 
     </html>
--- javascript/focusedPost.js
+++ javascript/focusedPost.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-import { getDatabase, ref, onValue, get, } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-database.js";
+import { getDatabase, ref, onValue, get, onChildAdded,onChildRemoved} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-database.js";
 
 
 const firebaseConfig = {
@@ -13,11 +13,13 @@ const firebaseConfig = {
   measurementId: "G-KT8PQT8XMT",
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
 
 const urlParams = new URLSearchParams(window.location.search);
 const postID = urlParams.get("postID");
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const startCountRef = ref(db, 'POSTS/'+postID+"/commentSection");
 
 
 if (!postID) {
@@ -28,9 +30,6 @@ if (!postID) {
     if (snapshot.exists()) {
       const post = snapshot.val();
       renderPost(post, snapshot.key);
-
-      renderComments(post, snapshot.key);
-
     } else {
       
     }
@@ -60,11 +59,40 @@ function renderPost(post, key) {
   });
   document.getElementById("likes").innerText = post.likes;
   document.getElementById("comments").innerText = post.comments;
-
-  renderComments(post.commentSection);
 }
-
-function renderComments (post) {
-    
-
-}
+  
+  
+  console.log(postID);
+  
+  
+  onChildAdded(startCountRef, comment=>{
+    addComment(comment);
+  })
+  
+  onChildRemoved(startCountRef, comment=>{
+    document.getElementById(comment.key).remove()
+  })
+  
+  function addComment(comment){
+    var nComment = document.createElement("div");
+    nComment.id = comment.key
+    nComment.className = "comment";
+    document.getElementById("commentSection").prepend(nComment);
+  
+    fetch('comment.html').then(response => response.text()).then(data => {
+        document.getElementById(nComment.id).innerHTML = data;
+  
+        document.getElementById('commentPic').id = "commentPic" + nComment.id;
+        document.getElementById('commentName').id = "commentName" + nComment.id;
+        document.getElementById('commentTime').id = "commentTime" + nComment.id;
+        document.getElementById('content').id = "content" + nComment.id;
+  
+  
+        document.getElementById("commentPic" + nComment.id).src = "" + comment.child("commenterProfileURL").val();
+        document.getElementById("commentName" + nComment.id).innerHTML = comment.child("commentuserName").val();
+        document.getElementById("commentTime" + nComment.id).innerHTML = comment.child("commentTime").val();
+        document.getElementById("content" + nComment.id).innerHTML = comment.child("commentDescription").val();
+  
+    })
+  }
+      
